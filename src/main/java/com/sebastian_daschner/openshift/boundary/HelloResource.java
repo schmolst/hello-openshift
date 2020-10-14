101a9a2e8d9d6f01cf3a5d6ad62a43ecf87b53c6
Steffen Schmolz (ls271pz) <steffen.schmolz@allianz.de>
2020-10-14 13:23:40 +0200

    feat: greetings with timestamp

--- src/main/java/com/sebastian_daschner/openshift/boundary/HelloResource.java
+++ src/main/java/com/sebastian_daschner/openshift/boundary/HelloResource.java
@@ -3,13 +3,14 @@ package com.sebastian_daschner.openshift.boundary;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import java.time.Instant;
+import java.time.LocalDate;
 
 @Path("hello")
 public class HelloResource {
 
     @GET
     public String hello() {
-        return "Hello my OpenShift";
+        return "Hello my OpenShift, Du siehst gut aus. Heute ist der " + LocalDate.now();
     }
 
 }
